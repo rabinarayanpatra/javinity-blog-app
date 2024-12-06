3ff2ec3407d08cc8d1ca0e6359bb52b67b768c2f
Rabi <rabinarayanpatra1999@gmail.com>
2024-12-07 01:37:55 +0530

    Initial Commit.

---
+++ src/main/java/com/javinity/JavinityApplication.java
@@ -0,0 +1,13 @@
+package com.javinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavinityApplication {
+
+  public static void main( final String[] args ) {
+    SpringApplication.run( JavinityApplication.class, args );
+  }
+
+}
---
+++ src/test/java/com/javinity/JavinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavinityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
